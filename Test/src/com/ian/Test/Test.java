5dee4763eb419f140ef5f82d66f33b0fc2d847f1
Ian Thompson <thepi200@gmail.com>
2012-09-12 19:01:07 -0400

    HW for 9/12

--- Test/src/com/ian/Test/Test.java
+++ Test/src/com/ian/Test/Test.java
@@ -7,7 +7,34 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-
+		
+		System.out.println(intSum(5));
+		System.out.println(intSum(1));
+		System.out.println(intSum(0));
+		System.out.println(intSum(-4));
+	}
+	
+	/**
+	 * Calculates the sum of all numbers from 1 to n
+	 * @param n The end number
+	 * @return The sum from 1 to n
+	 */
+	public static int intSum(int n)
+	{
+		int sum = 0;
+		
+		if (n < 1)
+		{ // Counting backwards
+			for(int i = 1; i >= n; i--)
+				sum += i;
+		}
+		else
+		{ // Counting forwards
+			for(int i = 1; i <= n; i++)
+				sum += i;
+		}
+		
+		return sum;
 	}
 
 }

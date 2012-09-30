c973f7282f1ec73aaa10c602aef42a5bfbb0bad8
Ian Thompson <thepi200@gmail.com>
2012-09-30 16:47:08 -0400

    Fancier comments!

--- UnitOne/src/CoordGeom.java
+++ UnitOne/src/CoordGeom.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 /**
  * Calculates various bits of info about a line segment
+ * 
  * @author Ian Thompson
- *
+ * 
  */
-public class CoordGeom {
-	
+public class CoordGeom
+{
+
 	/**
 	 * Prints the slope of segment AB
+	 * 
 	 * @param Ax The x-coordinate of point A
 	 * @param Ay The y-coordinate of point A
 	 * @param Bx The x-coordinate of point B
@@ -26,9 +29,10 @@ public class CoordGeom {
 			System.out.println("The slope of segment AB is " + slope);
 		}
 	}
-	
+
 	/**
 	 * Prints the length of segment AB
+	 * 
 	 * @param Ax The x-coordinate of point A
 	 * @param Ay The y-coordinate of point A
 	 * @param Bx The x-coordinate of point B
@@ -36,12 +40,13 @@ public class CoordGeom {
 	 */
 	private static void printLength(double Ax, double Ay, double Bx, double By)
 	{
-		double length = Math.sqrt(Math.pow(Ax - Bx, 2) + Math.pow(Ay -By, 2));
+		double length = Math.sqrt(Math.pow(Ax - Bx, 2) + Math.pow(Ay - By, 2));
 		System.out.println("The length of segment AB is " + length);
 	}
-	
+
 	/**
 	 * Prints the midpoint of segment AB
+	 * 
 	 * @param Ax The x-coordinate of point A
 	 * @param Ay The y-coordinate of point A
 	 * @param Bx The x-coordinate of point B
@@ -51,13 +56,14 @@ public class CoordGeom {
 	{
 		double midX = (Ax + Bx) / 2;
 		double midY = (Ay + By) / 2;
-	
-		System.out.println("The midpoint of segment AB is (" + midX + ", "
-				+ midY + ")");
+
+		System.out.println("The midpoint of segment AB is (" + midX + ", " + midY
+				+ ")");
 	}
-	
+
 	/**
 	 * Prints the minimum x-coordinate of segment AB
+	 * 
 	 * @param Ax The x-coordinate of point A
 	 * @param Ay The y-coordinate of point A
 	 * @param Bx The x-coordinate of point B
@@ -70,12 +76,13 @@ public class CoordGeom {
 			minX = Bx;
 		else
 			minX = Ax;
-		
+
 		System.out.println("The minimum x-coordinate of all of AB is " + minX);
 	}
 
 	/**
 	 * The entry point. Retrieves the coordinates from the console
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args)
@@ -84,13 +91,13 @@ public class CoordGeom {
 		System.out.print("Input point A: ");
 		double Ax = sc.nextDouble();
 		double Ay = sc.nextDouble();
-		
+
 		System.out.print("Input point B: ");
 		double Bx = sc.nextDouble();
 		double By = sc.nextDouble();
-		
+
 		sc.close();
-		
+
 		printSlope(Ax, Ay, Bx, By);
 		printLength(Ax, Ay, Bx, By);
 		printMidpoint(Ax, Ay, Bx, By);
